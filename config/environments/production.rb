c0e2cc78e02ac03850dad3a39218085635437e73
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-07-14 15:29:10 -0400

    added SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,28 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  #HEROKU CONFIG
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.action_mailer.default_url_options = { host: 'https://delicat-saucisson-10989.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'michael.james.randell@gmail.com',
+    :password => 'sherlock12345',
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
